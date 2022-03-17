ac1cf46d52cd7bcd51a9b226440a6f162bb1a395
DaniellaMolina <lic.daniellamolina@gmail.com>
2022-03-16 21:18:48 -0300

    Desafio 1

---
+++ javascript/entregas/clase3.js
@@ -0,0 +1,12 @@
+let producto = prompt ("Ingrese si quiere comprar lemon pie, red velvet o chocotorta");
+
+    if (producto == "lemon pie") {
+        alert ("El lemon pie cuesta $990")
+    } else if (producto == "red velvet"){
+        alert ("La torta red velvet cuesta $1050")
+    }else if (producto == "chocotorta"){
+        alert ("La chocotorta cuesta $970")
+    }
+
+
+alert ("Gracias por su compra, lo esperamos pronto nuevamente.");
