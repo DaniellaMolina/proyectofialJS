813e26730e251e421cabf1d3f5c506b487f7bcf5
DaniellaMolina <lic.daniellamolina@gmail.com>
2022-04-02 17:43:59 -0300

    Desafio 3

--- javascript/entregas/desafio3.js
+++ javascript/entregas/desafio3.js
@@ -1,25 +1,64 @@
-function Datospersonales1 (nombrecompleto, edad, genero, estadocivil, mail){
+//Array filtrar//
+/*let arreglo =[1,2,3,4,5,6,7,8,9,10]; 
+let pares= arreglo.filter(function(numero){  
+  return numero % 2 ==0; 
+});
 
-    this.nombrecompleto = nombrecompleto;
-    this.genero =  genero;
-    this.edad = edad;
-    this.estadocivil = estadocivil;
-    this.mail = mail;
+console.log (pares)*/
 
-}
+//Array de productos//
+const productos = [
+    {nombre: 'Lemon pie',    precio: 990},
+    {nombre: 'Chesee cake',       precio: 1100},
+    {nombre: 'Chocomouse',    precio: 1500},
+    {nombre: 'Choco en rama',     precio: 1300},
+    {nombre: 'Frutos rojos y chocolate',   precio: 1200},
+    {nombre: 'Torta de caramelo',     precio: 1000},
+    {nombre: 'Macarron cake',     precio: 1850},
+    {nombre: 'Strawberry and cream',  precio: 890}
+]
+
+//Recorrer el array y obtener cada producto//
+productos.forEach((producto) => {
+    console.log(producto.nombre)
+});
 
 
-const Datospersonales2 =  new Datospersonales1("Daniella Molina", 28, "femenino", "soltera","lic.daniellamolina@gmail.com");
-
-let nombrecompleto = prompt("Ingrese su nombre completo");
-let genero = prompt("Ingrese su edad");
-let edad= parseInt(prompt("Ingrese el genero con el cual se identifica"));
-let estadocivil = prompt("Ingrese su estado civil");
-let mail = prompt ("Ingrese su mail")
+//Ordena el array por menor a mayor precio//
+console.log(productos.sort((a, b) => a.precio - b.precio));
 
 
-const Datospersonales3 =  new Datospersonales1(nombrecompleto, edad, genero, estadocivil, mail);
+// Filtrar un array con precio menor a $1200//
+const productosFiltrados = productos.filter((producto) => {
+    return producto.precio <= 1200
+});
 
-console.log (Datospersonales1);
-console.log (Datospersonales2);
-console.log (Datospersonales3);
+console.log (productosFiltrados);
+
+
+//Map: Obtener un nuevo array con los nombres de los productos//
+const nombresProductos = productos.map((producto) => {
+    return producto.nombre
+});
+console.log (nombresProductos);
+
+
+//Find: buscar un producto, ejemplo lemon pie//
+const busquedaProducto = productos.find((producto) => {
+    return producto.nombre === "Lemon pie"
+})
+
+console.log (busquedaProducto);
+
+//Buscar si existe un chesee cake//
+const incluyeChesee = productos.includes('Chesee cake')
+
+console.log (incluyeChesee);
+
+//Length//
+console.log (productos.length);
+
+//Push//
+productos.push ('Chocotorta')
+console.log (productos);
+
