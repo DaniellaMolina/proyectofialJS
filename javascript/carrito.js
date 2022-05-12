49267437817dda8be1ad27e976d153b4d0726130
DaniellaMolina <lic.daniellamolina@gmail.com>
2022-05-12 20:03:36 -0300

    Carrito

--- javascript/carrito.js
+++ javascript/carrito.js
@@ -1,245 +1,8 @@
-// Constantes y variables
-const cakes = [
-  {
-    id: 0,
-    nombre: "Lemon Pie",
-    descripcion: "Torta de limon con merengue italiano",
-    precio: 990,
-    img: "/images/30.png",
-  },
-  {
-    id: 1,
-    nombre: "Cheese cake",
-    descripcion: "Torta de mouse de queso con frutos rojos",
-    precio: 1100,
-    img: "/images/31.png",
-  },
-  {
-    id: 2,
-    nombre: "Chocomouse",
-    descripcion: "Torta de bizcochuelo con mouse de chocolate y nutella",
-    precio: 1500,
-    img: "/images/32.png",
-  },
-  {
-    id: 3,
-    nombre: "Choco en rama",
-    descripcion: "Torta con chantilly y chocolate en rama",
-    precio: 1300,
-    img: "/images/33.png",
-  },
-  {
-    id: 4,
-    nombre: "Frutos rojos y chocolate",
-    descripcion:
-      "Mouse de chocolate blanco con una ganache de chocolate negro y frutos rojos",
-    precio: 1200,
-    img: "/images/34.png",
-  },
-  {
-    id: 5,
-    nombre: "Torta Caramelo",
-    descripcion: "Torta con base de caramelo",
-    precio: 1050,
-    img: "/images/35.png",
-  },
-  {
-    id: 6,
-    nombre: "Macarron cake",
-    descripcion: "Torta con toppings de macarons",
-    precio: 1500,
-    img: "/images/36.png",
-  },
-  {
-    id: 7,
-    nombre: "Strawberry and cream",
-    descripcion: "Torta con base de bizcochuelo, con butter cream y frutillas ",
-    precio: 890,
-    img: "/images/37.png",
-  },
-  {
-    id: 8,
-    nombre: "Arcoiris",
-    descripcion: "Torta con base de de bizcochuelo de colores pasteles",
-    precio: 1500,
-    img: "/images/38.png",
-  },
-  {
-    id: 9,
-    nombre: "Primavera",
-    descripcion: "Torta con butter cream y flores comestibles",
-    precio: 1200,
-    img: "/images/39.png",
-  },
-  {
-    id: 10,
-    nombre: "Torta menta",
-    descripcion: "Torta de chocolate y menta",
-    precio: 990,
-    img: "/images/40.png",
-  },
-  {
-    id: 11,
-    nombre: "Macarron gigante",
-    descripcion: "Macarron gigante con frutos del bosque",
-    precio: 890,
-    img: "/images/41.png",
-  },
-  {
-    id: 12,
-    nombre: "Red Velvet",
-    descripcion: "Bizcochuelo rojo con butter cream y frutos del bosque",
-    precio: 1200,
-    img: "/images/42.png",
-  },
-  {
-    id: 13,
-    nombre: "Arandanos",
-    descripcion: "Bizcochuelo relleno de duce de leche con una butter cream de arandanos",
-    precio: 1400,
-    img: "/images/43.png",
-  },
-  {
-    id: 14,
-    nombre: "Kit Kat",
-    descripcion: "Bizcochuelo relleno de duce de leche con Kit Kat",
-    precio: 1400,
-    img: "/images/44.png",
-  },
-  {
-    id: 15,
-    nombre: "Oreo",
-    descripcion: "Bizcochuelo relleno de duce de leche con butter cream y oreos",
-    precio: 1400,
-    img: "/images/45.png",
-  },
-  {
-    id: 16,
-    nombre: "Torta citrica",
-    descripcion:
-      "Bizcochuelo relleno de mouse de naranja con butter cream naranja y crema inglesa",
-    precio: 1050,
-    img: "/images/46.png",
-  },
-  {
-    id: 17,
-    nombre: "Reversion de lemon pie",
-    descripcion:
-      "Bizcochuelo relleno de crema pastelera de limon con merengue italiano flambeado",
-    precio: 950,
-    img: "/images/47.png",
-  },
-  {
-    id: 18,
-    nombre: "Butter cream cake",
-    descripcion:
-      "Bizcochuelo relleno de crema citrica y dulce de leche con merengues italianos",
-    precio: 1200,
-    img: "/images/48.png",
-  },
-  {
-    id: 19,
-    nombre: "Nutella",
-    descripcion: "Bizcochuelo relleno de nutella",
-    precio: 1500,
-    img: "/images/49.png",
-  },
-  {
-    id: 20,
-    nombre: "Menta y cream",
-    descripcion: "Bizcochuelo relleno de crema de menta con butter cream de jengibre",
-    precio: 950,
-    img: "/images/50.png",
-  },
-  {
-    id: 21,
-    nombre: "Spring Cake",
-    descripcion:
-      "Bizcochuelo relleno de crema pastelera de frambuesa con mermelada de arandanos",
-    precio: 1250,
-    img: "/images/51.png",
-  },
-  {
-    id: 22,
-    nombre: "Violet",
-    descripcion: " ",
-    precio: 1200,
-    img: "/images/52.png",
-  },
-  {
-    id: 23,
-    nombre: "Choco pasión",
-    descripcion: "",
-    precio: 950,
-    img: "/images/53.png",
-  },
-  {
-    id: 24,
-    nombre: "Carrot cake",
-    descripcion: "",
-    precio: 1200,
-    img: "/images/54.png",
-  },
-  {
-    id: 25,
-    nombre: "Torta Alfajor",
-    descripcion: "",
-    precio: 1300,
-    img: "/images/55.png",
-  },
-  {
-    id: 26,
-    nombre: "Chajá",
-    descripcion: "",
-    precio: 1400,
-    img: "/images/56.png",
-  },
-  {
-    id: 27,
-    nombre: "Coffee Cake",
-    descripcion: "",
-    precio: 1200,
-    img: "/images/57.png",
-  },
-  {
-    id: 28,
-    nombre: "Torta Salchichón de chocolate",
-    descripcion: "",
-    precio: 1300,
-    img: "/images/58.png",
-  },
-  {
-    id: 28,
-    nombre: "Laberinto",
-    descripcion: "",
-    precio: 1300,
-    img: "/images/59.png",
-  },
-  {
-    id: 29,
-    nombre: "Valentin's cake",
-    descripcion: "",
-    precio: 1200,
-    img: "/images/60.png",
-  },
-  {
-    id: 30,
-    nombre: "Chocotorta con Ferrero Rocher",
-    descripcion: "",
-    precio: 1500,
-    img: "/images/61.png",
-  },
-];
+// ----- Imprimir productos en el HTML, convertir un archivo json a datos javascript----- //
+const cakes = "./productos.json";
 
-// ----- Chquear carrito en Storage ----- //
-
-// Si existe me guarda en la variable, sino lo deja vacio.
-let carrito = JSON.parse(localStorage.getItem("carritoEnStorage")) || [];
-console.log(carrito);
-dibujarTabla(carrito);
-
-// ----- Imprimir productos en el HTML ----- //
-for (let cake of cakes) {
+fetch(cakes).then(res => res.json).then( data => { 
+  for (let cake of cakes) {
   $("#cuerpo").append(`
             <div class="card text-center" style="width: 15rem; margin:1rem;">
             <div class="card-body">
@@ -252,8 +15,17 @@ for (let cake of cakes) {
                 </div>
             </div>
         </div>
-        `);
-}
+        `);} 
+})
+
+
+// Si existe me guarda en la variable, sino lo deja vacio.
+let carrito = JSON.parse(localStorage.getItem("carritoEnStorage")) || [];
+console.log(carrito);
+dibujarTabla(carrito);
+
+
+
 
 // ----- Carrito ----- //
 // ----- Tabla del carrito ----- //
